3912eb5c67515876475d9bdfbe10a012d951926e
Andrew Hegman <arhegman@gmail.com>
2021-01-01 17:18:57 -0500

    Removing redux devtools from prod

--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -3,9 +3,12 @@ import { rootReducer } from "./Store/index";
 import { applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
-const store = createStore(
-  rootReducer,
-  compose(applyMiddleware(thunk), (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__({ trace: true }))
-);
+const composeEnhancers =
+  (process.env.REACT_APP_IS_PROD === "false" &&
+    typeof window !== "undefined" &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true })) ||
+  compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export { store };
